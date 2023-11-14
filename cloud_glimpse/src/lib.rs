86ad02469f85a1fff44f9895f2c24ab3acde0a6f
NeuralNuts <wilsonzakk8@gmail.com>
2023-11-15 00:40:08 +1000

    Changed UI

--- cloud_glimpse/src/lib.rs
+++ cloud_glimpse/src/lib.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
-use bevy::prelude::Resource;
+use bevy::prelude::{Resource, Window};
 use wasm_bindgen::prelude::*;
+use bevy::window::WindowPlugin;
 use bevy::render::render_resource::{AsBindGroup, PrimitiveTopology, ShaderRef};
 use bevy_panorbit_camera::PanOrbitCameraPlugin;
 use bevy_panorbit_camera::PanOrbitCamera;
@@ -38,7 +39,7 @@ impl LasFileData {
 }
 
 #[wasm_bindgen]
-pub fn main(file_data: &[u8]) {
+pub fn main(file_data: &[u8]) { 
     let point_records = match read_las_file(file_data) {
         Ok(points) => points,
         Err(err) => {
@@ -51,6 +52,7 @@ pub fn main(file_data: &[u8]) {
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(LasFileData::new(point_records))
         .add_plugins(DefaultPlugins)
+        .add_plugin(bevy_webgl2::WebGL2Plugin)
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
         .run();
--- cloud_glimpse/src/website/index.js
+++ cloud_glimpse/src/website/index.js
@@ -1,20 +1,55 @@
 import init from '/cloud_glimpse/pkg/cloud_glimpse.js';
 import { main } from '/cloud_glimpse/pkg/cloud_glimpse.js'; // Adjust the path as needed
 
+async function runMainWithFile(file) {
+    const arrayBuffer = await file.arrayBuffer();
+    main(new Uint8Array(arrayBuffer));
+        // It's assumed that main or another function from your WASM will handle
+        // the canvas creation or manipulation, and that the canvas will end up
+        // inside `#canvas-container` or replaces an existing `<canvas>` element.
+}
+
 init().then(() => {
     console.log("WASM module loaded and initialized");
 
-    // Register the click event handler for the button by its id
     const loadButton = document.getElementById('load-button');
-    loadButton.addEventListener('click', async () => {
-        const input = document.getElementById('file-input');
-        if (!input.files.length) return;
+    loadButton.addEventListener('click', () => {
+        document.getElementById('file-input').click();
+    });
 
-        const file = input.files[0];
-        const arrayBuffer = await file.arrayBuffer();
+    const fileInput = document.getElementById('file-input');
+    fileInput.addEventListener('change', async () => {
+        if (!fileInput.files.length) return;
 
-        main(new Uint8Array(arrayBuffer));
+        const file = fileInput.files[0];
+
+        runMainWithFile(file);
+        moveCanvasToDiv();
     });
 }).catch(e => {
     console.error("Failed to initialize the WASM module:", e);
 });
+
+function moveCanvasToDiv() {
+    let div = document.getElementById("bevy-canvas");
+
+    // Create a mutation observer to notify us when the canvas is added to the DOM
+    const observer = new MutationObserver((mutations, obs) => {
+        const canvas = document.querySelector("canvas");
+        if (canvas) {
+            div.appendChild(canvas);
+            console.log("Canvas moved to div.");
+            obs.disconnect(); // Stop observing once the canvas is moved
+        }
+    });
+
+    // Observe the entire document
+    observer.observe(document.body, {
+        childList: true,  // observe direct children
+        subtree: true,    // and lower descendants too
+        attributes: false,
+        characterData: false,
+    });
+
+    console.log("Observer set up to move canvas when available.");
+}
