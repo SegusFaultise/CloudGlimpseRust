31602a2c8de2efb4c78936e28dacdf0e6f745371
NeuralNuts <wilsonzakk8@gmail.com>
2023-11-17 00:04:12 +1000

    Added XML comment to lib.rs

--- cloud_glimpse/src/lib.rs
+++ cloud_glimpse/src/lib.rs
@@ -3,11 +3,8 @@ use bevy::prelude::{Resource, Window};
 use bevy::window::WindowPlugin;
 use bevy::render::render_resource::{AsBindGroup, PrimitiveTopology, ShaderRef};
 use bevy_panorbit_camera::PanOrbitCameraPlugin;
-use bevy_panorbit_camera::PanOrbitCamera;
 
 use wasm_bindgen::prelude::*;
-
-use std::path::Path;
 
 mod las_file_handler;
 use las_file_handler::las_file_parser::{read_las_file, Point3D, get_total_points};
@@ -16,18 +13,39 @@ mod las_visualization;
 use las_visualization::point_creation::{create_point_mesh_from_point3d, height_to_color};
 use las_visualization::bevy_setup::{setup, LasFileData};
 
+/// A material for rendering points, containing color information.
+///
+/// This struct is used to define how points are rendered, including their color.
+/// It binds a color to a uniform in the shader.
 #[derive(AsBindGroup, Debug, Clone, Asset, TypePath)]
 pub struct PointMaterial {
+
+    /// The color of the points. This is bound to the shader as a uniform.
     #[uniform(0)]
     color: Color,
 }
 
+/// Implementation of the `Material` trait for `PointMaterial`.
+///
+/// This provides the necessary shader reference for rendering.
 impl Material for PointMaterial {
+
+    /// Specifies the fragment shader to use for rendering points.
+    ///
+    /// # Returns
+    /// Returns a reference to the shader file.
     fn fragment_shader() -> ShaderRef {
         "shaders/PointMaterial.wgsl".into()
     }
 }
 
+/// The entry point for the WebAssembly application.
+///
+/// This function is called when the WebAssembly module is executed.
+/// It reads LAS file data, initializes the application, and runs it.
+///
+/// # Arguments
+/// * `file_data` - A byte slice reference to the contents of a LAS file.
 #[wasm_bindgen]
 pub fn main(file_data: &[u8]) { 
     let point_records = match read_las_file(file_data) {
