06bc61d6521102c0594ca97d11abe21784e13795
NeuralNuts <wilsonzakk@gmail.com>
2024-01-23 19:14:41 +1000

    misc

--- cloud_glimpse/serve.py
+++ cloud_glimpse/serve.py
@@ -1,6 +1,17 @@
 import http.server
 import socketserver
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
@@ -11,5 +22,6 @@ Handler.extensions_map = {
 }
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    print(f"{bcolors.OKGREEN}[SUCCESS]:{bcolors.ENDC} serving at, {bcolors.UNDERLINE + bcolors.OKCYAN}http://localhost:{PORT}/src/website/index.html{bcolors.ENDC}")
+
     httpd.serve_forever()
